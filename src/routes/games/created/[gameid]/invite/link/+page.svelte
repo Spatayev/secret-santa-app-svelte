cac2d210a087c1a012665563ac21eeaa241d128f
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-05-01 01:55:14 +0500

    add copy mem funct

--- src/routes/games/created/[gameid]/invite/link/+page.svelte
+++ src/routes/games/created/[gameid]/invite/link/+page.svelte
@@ -1,13 +1,44 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	export let data;
-	console.log(data);
+
+	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
+  
+	const url = writable(data.res.link);
+	const copied = writable(false);
+  
+	onMount(async () => {
+	  if (data && data.link) {
+		const modifiedUrl = data.link.replace(/\/invitations\/accept\//, "/invitations_accept/");
+		url.set(modifiedUrl);
+	  }
+	});
+  
+	const handleCopyToClipboard = () => {
+	  navigator.clipboard.writeText($url);
+	  copied.set(true);
+	  setTimeout(() => {
+		copied.set(false);
+	  }, 2000);
+	};
+
 </script>
 
 <section>
-	<label for="invite">
-		<input type="url" name="invite" value={data.res.link} readonly />
-	</label>
+
+
+
+	<div class="container">
+		<h3 align="center">Пригласить участников</h3>
+		<p align="center">Скопируйте ссылку ниже и отправьте её своим друзьям.</p>
+		<p align="center">После перехода по ссылке, участники смогут создать карточки для участия самостоятельно.</p>
+		<div class="styled-box">
+			<input type="text" bind:value={$url} readonly>
+			<button class="main-button" on:click={handleCopyToClipboard}>{#if $copied}Скопировано!{:else}Копировать{/if}</button>
+		</div>
+		</div>
+
 	<div>
 		<button
 			on:click={() => {
