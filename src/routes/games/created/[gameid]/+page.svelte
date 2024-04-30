d01cc4dbc44fc5c1b1ad507d6a2f781081fb81d5
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-05-01 01:03:02 +0500

    add: param url game id

---
+++ src/routes/games/created/[gameid]/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	export let data;
+</script>
+
+<section>
+	<div>
+		<h2>Игра создана</h2>
+	</div>
+	<div role="img"></div>
+	<div>
+		<h4>Пока что никого нет</h4>
+	</div>
+	<div>
+		<button
+			on:click={() => {
+				goto('created/invite');
+			}}>Добавить участников</button
+		>
+	</div>
+	{data}
+</section>
---
+++ src/routes/games/created/[gameid]/invite/card/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<section>
+	<form action="POST">
+		<label for=""></label><label for=""></label><label for=""></label>
+	</form>
+</section>
