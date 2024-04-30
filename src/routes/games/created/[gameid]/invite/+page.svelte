daf979b75279e55f936c2763303d494651a1ce19
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-05-01 02:55:38 +0500

    Update +page.svelte

--- src/routes/games/created/[gameid]/invite/+page.svelte
+++ src/routes/games/created/[gameid]/invite/+page.svelte
@@ -1,53 +1,20 @@
-<script>
-    import { onMount } from 'svelte';
-
-    let participants = [{ name: '', email: '' }];
-    let ID = '';
-    let access = '';
-
-    onMount(() => {
-        const params = new URLSearchParams(window.location.search);
-        ID = params.get('game_id') || '';
-
-    });
-
-    const handleAddParticipant = () => {
-        participants = [...participants, { name: '', email: '' }];
-    };
-
-    const handleInputChange = (index, event) => {
-        const { name, value } = event.target;
-        participants[index][name] = value;
-        participants = [...participants];
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const serializedFormData = Object.fromEntries(formData.entries());
-        console.log(serializedFormData);
-    };
+<script lang="ts">
+	import { goto } from '$app/navigation';
 </script>
 
-<style>
-
-</style>
-
-<section class="styled-box">
-    <form method="POST" action="?/sender">
-        <h5>Добавить участников</h5>
-        {#each participants as participant, index}
-        <div>
-            Участник №{index + 1}
-            <input type="text" placeholder="Имя" name="name" value={participant.name} on:input={(e)=>
-            handleInputChange(index, e)} />
-            <input type="text" placeholder="E-mail" name="email" value={participant.email} on:input={(e)=>
-            handleInputChange(index, e)} />
-        </div>
-        {/each}
-        <input type="hidden" name="gameId" value={ID} />
-
-        <button type="button" on:click={handleAddParticipant}>Добавить еще участника</button>
-        <button type="submit">Пригласить</button>
-    </form>
+<section>
+	<div>
+		<h2>Добавить участников</h2>
+	</div>
+	<div><button>Создать свою карточку участника</button></div>
+	<div><button on:click={() => {
+				goto('invite/send');
+			}}>Добавить участников вручную</button></div>
+	<div>
+		<button
+			on:click={() => {
+				goto('invite/link');
+			}}>Пригласить по ссылке</button
+		>
+	</div>
 </section>
