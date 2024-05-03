f45d5cdd59fb640b46db70d9c4f5e684ac7ac624
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-05-03 13:57:42 +0500

    refactor: add ten field static

--- src/routes/games/created/[gameid]/invite/send/+page.svelte
+++ src/routes/games/created/[gameid]/invite/send/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-    import { onMount } from 'svelte';
     import { superForm } from 'sveltekit-superforms';
 
     export let data;
@@ -7,26 +6,7 @@
     // Client API:
     const { form, errors, message } = superForm(data.form);
 
-    let participants = [{ name: '', email: '' }];
 
-
-
-    const handleAddParticipant = () => {
-        participants = [...participants, { name: '', email: '' }];
-    };
-
-    const handleInputChange = (index, event) => {
-        const { name, value } = event.target;
-        participants[index][name] = value;
-        participants = [...participants];
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const serializedFormData = Object.fromEntries(formData.entries());
-        console.log(serializedFormData);
-    };
 </script>
 
 <style>
@@ -36,24 +16,69 @@
 <section class="styled-box">
     <form method="POST" action="?/sender">
         <h5>Добавить участников</h5>
-        {#each participants as participant, index}
         <div>
-            Участник №{index + 1}
-            <input type="text" placeholder="Имя" name={`name${index}`} value={participant.name} on:input={(e)=>
-            handleInputChange(index, e)} />
-            {#if $errors.name}
-            <small>{$errors.name}</small>
+            Участник №{1}
+            <input type="text" placeholder="Имя" name={`name${0}`} bind:value={$form.name0} />
+            {#if $errors.name0}
+            <small>{$errors.name0}</small>
             {/if}
 
-            <input type="text" placeholder="E-mail" name={`email${index}`} value={participant.email} on:input={(e)=>
-            handleInputChange(index, e)} />
-            {#if $errors.email}
-            <small>{$errors.email}</small>
+            <input type="text" placeholder="E-mail" name={`email${0}`} bind:value={$form.email0}  />
+            {#if $errors.email0}
+            <small>{$errors.email0}</small>
             {/if}
+
+            Участник №{2}
+            <input type="text" placeholder="Имя" name={`name${1}`} bind:value={$form.name1} />
+            {#if $errors.name1}
+            <small>{$errors.name1}</small>
+            {/if}
+
+            <input type="text" placeholder="E-mail" name={`email${1}`} bind:value={$form.email1}  />
+            {#if $errors.email1}
+            <small>{$errors.email1}</small>
+            {/if}
+
+            Участник №{3}
+            <input type="text" placeholder="Имя" name={`name${2}`} bind:value={$form.name2} />
+            {#if $errors.name2}
+            <small>{$errors.name2}</small>
+            {/if}
+
+            <input type="text" placeholder="E-mail" name={`email${2}`} bind:value={$form.email2}  />
+            {#if $errors.email2}
+            <small>{$errors.email2}</small>
+            {/if}
+
+
+            Участник №{4}
+            <input type="text" placeholder="Имя" name={`name${3}`} bind:value={$form.name3} />
+            {#if $errors.name3}
+            <small>{$errors.name3}</small>
+            {/if}
+
+            <input type="text" placeholder="E-mail" name={`email${3}`} bind:value={$form.email3}  />
+            {#if $errors.email3}
+            <small>{$errors.email3}</small>
+            {/if}
+
+
+            Участник №{5}
+            <input type="text" placeholder="Имя" name={`name${4}`} bind:value={$form.name4} />
+            {#if $errors.name4}
+            <small>{$errors.name4}</small>
+            {/if}
+
+            <input type="text" placeholder="E-mail" name={`email${4}`} bind:value={$form.email4}  />
+            {#if $errors.email4}
+            <small>{$errors.email4}</small>
+            {/if}
+
+
+
         </div>
-        {/each}
 
-        <button type="button" on:click={handleAddParticipant}>Добавить еще участника</button>
+        <button type="button">Добавить еще участника</button>
         <button type="submit">Пригласить</button>
 
         {#if $message}
