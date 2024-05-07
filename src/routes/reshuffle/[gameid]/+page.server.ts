a81be65ad2f69ee0e78f31b8170fa59ae1fd0790
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-05-07 23:28:40 +0300

    add server private env
    import { sess_cookie_name } from '$env/static/private';
    
    
     
    export const load = async () => {
        console.log('sess_cookie_name');
        console.log(sess_cookie_name);
        
        
        
    	return {  };
    };

---
+++ src/routes/reshuffle/[gameid]/+page.server.ts
@@ -0,0 +1,12 @@
+import { sess_cookie_name } from '$env/static/private';
+
+
+ 
+export const load = async () => {
+    console.log('sess_cookie_name');
+    console.log(sess_cookie_name);
+    
+    
+    
+	return {  };
+};
