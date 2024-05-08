739c8a6e789977533fe09367344777b3e3cd029e
Knm24 <143102406+Knm24@users.noreply.github.com>
2024-05-09 00:49:17 +0500

    refactor: reshuffle
     Проверку на автора ввести и минимум на двоих игроков

--- src/routes/reshuffle/[gameid]/+page.server.ts
+++ src/routes/reshuffle/[gameid]/+page.server.ts
@@ -1,5 +1,15 @@
 import { sess_cookie_name } from '$env/static/private';
+import { fail, message, superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
 
+import { PUBLIC_BASE_URL, PUBLIC_language } from '$env/static/public';
+
+
+
+// Define outside the load function so the adapter can be cached
+const schema = z.object({
+});
 
  
 export const load = async () => {
@@ -7,6 +17,72 @@ export const load = async () => {
     console.log(sess_cookie_name);
     
     
+	const form = await superValidate(zod(schema));
+
+	// Always return { form } in load functions
+	return { form };
+};
+
+
+export const actions = {
+	start_game: async ({ request, params, cookies  }) => {
+		const form = await superValidate(request, zod(schema));
+
+		console.log('start_game');
+		console.log(cookies);
+
+        console.log('gameid');
+
+		console.log(params.gameid);
+
+		
+
+
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+		
+		try {
+		const response = await fetch(PUBLIC_BASE_URL + '/games/' + params.gameid + '/reshuffle/', {
+			method: 'POST',
+			headers: {
+				'accept': '*/*',
+				Authorization: `Bearer ${cookies.get('accessToken')}`,
+			},
+			body: JSON.stringify({})
+		});
+		console.log('response accept');
+		console.log(response);
+
+		if (!response.ok) {
+			const errorText = await response.text();
+			return message(form, errorText);
+		}
+		else {
+			const successText = await response.text();
+			return message(form, successText);
+		}
+
+	} catch(error) {
+		return message(form, error.message || 'reshuffle: Internal Server Error 500');
+
+	}
+  },
+
+  show_all_user: async ({ request, params, cookies  }) => {
+
     
-	return {  };
+		console.log('show_all_user');
+		console.log(cookies);
+
+
+        console.log('список участников до жеребьевки');
+        console.log('/gameuser/{gameId}/list-before-shuffle');
+
+        console.log('список участников после жеребьевки');
+        console.log('/gameuser/{gameId}/list-after-shuffle');
+
+
+        
+  }
 };
--- src/routes/reshuffle/[gameid]/+page.svelte
+++ src/routes/reshuffle/[gameid]/+page.svelte
@@ -1,5 +1,11 @@
 
 <script>
+	import { superForm } from 'sveltekit-superforms';
+
+	export let data;
+
+	// Client API:
+	const { form, errors, message } = superForm(data.form);
 
   import { page } from '$app/stores';
 
@@ -15,27 +21,79 @@ import { PUBLIC_BASE_URL, PUBLIC_language } from '$env/static/public';
 
 
   console.log($page.params);
+
+  let ShuffleStarted = false;
+  let showUsers = false;
+  let isAuthor = false;
+
+// https://github.com/wobsoriano/svelte-sonner
+  import { Toaster, toast } from 'svelte-sonner'
+
+
 </script>
 
 <div class="container">
-  <div class="styled-box">
 
-    <form>
-      <div class="grid-container">
-        <h5 class="text-center">Жеребьевка. Название игры</h5>
+      <div>
+        <h5 class="text-center">Жеребьёвка.</h5>
         <p>Связаться с Организатором</p>
-      </div>
- <div class="grid-container">
-play ?
       </div>
 
 
       <div class="stack">
         <a href="/wishlist/{gameid}/my-giftee-wishlist">Узнать подопечного</a>
       </div>
+
+
+
+	  <form method="POST" action="?/start_game">
+
+
+
+
+		<div> Проверку на автора ввести и минимум на двоих игроков
+			<button type="submit">{ShuffleStarted ? "Идёт жеребьевка" : "Начать жеребьевку"}</button>
+		</div>
+
+            
+		{#if $message}
+			<div>{$message}</div>
+		{/if}
+
+
     </form>
+
+
+	  <form method="POST" action="?/show_all_user">
+
+
+
+
+		<div> Проверку на автора ввести и минимум на двоих игроков
+			<button type="submit">Показать всех игроков</button>
+		</div>
+
+            
+		{#if $message}
+			<div>{$message}</div>
+		{/if}
+
+
+    </form>
+
+
+
+
+<button on:click={() => toast.success('Нажал My first toast')}>
+  Нажми получи ответ
+</button>
+
+<button on:click={() => toast.error('Нажал My Error toast')}>
+  Нажми получи ошибочный ответ
+</button>
+<Toaster richColors  />
+
   </div>
-</div>
 
 <style>
 
