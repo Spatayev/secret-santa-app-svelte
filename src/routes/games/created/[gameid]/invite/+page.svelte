dae9ccc97087e22b8a74bfc64a1757d7ce9de285
Danial-Doskuzhinov <doskuzhinovdanial@gmail.com>
2024-05-14 20:15:52 +0500

    contact-info copied to card

--- src/routes/games/created/[gameid]/invite/+page.svelte
+++ src/routes/games/created/[gameid]/invite/+page.svelte
@@ -6,8 +6,16 @@
 	<section class='form-section'>
 		<article class='title-section'>
 			<h2>Добавить участников</h2>
+			
 		</article>
-		<div class='center variants'><button class='primary-btn'>Создать свою карточку участника</button></div>
+		<div class='center variants'>
+			<button class='primary-btn' on:click={() => {
+				goto('invite/card');
+			}}
+			>Создать свою карточку участника
+
+		</button>
+		</div>
 		<div class='center variants'><button class='primary-btn' on:click={() => {
 					goto('invite/send');
 				}}>Добавить участников вручную</button></div>
