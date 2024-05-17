54d8b2638d058acd87e5da5b2d1bac912dccb108
Spatayev <40237017+Spatayev@users.noreply.github.com>
2024-05-18 00:39:34 +0500

    Случайно удалил

--- src/routes/games/created/[gameid]/+page.server.ts
+++ src/routes/games/created/[gameid]/+page.server.ts
@@ -2,14 +2,13 @@ import type { PageServerLoad } from './$types';
 import { BASE_URL } from '$env/static/private';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
-	const response = await fetch( `${BASE_URL}gameuser/${params.gameid}/list-before-shuffle`, {
-			method: 'GET',
-			headers: {
-				Authorization: `Bearer ${cookies.get('accessToken')}`,
-				'Content-Type': 'application/json'
-			}
+	const response = await fetch(`${BASE_URL}gameuser/${params.gameid}/list-before-shuffle`, {
+		method: 'GET',
+		headers: {
+			Authorization: `Bearer ${cookies.get('accessToken')}`,
+			'Content-Type': 'application/json'
 		}
-	);
+	});
 	if (!response.ok) {
 		const badRes = await response.text();
 		console.log('badres', badRes);
@@ -17,7 +16,8 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
 	}
 	const result = await response.json();
 	const gameid = params.gameid;
-	console.log(result);
 	const isAcceptable = result.filter((ready) => ready.invitationStatus === 'ACCEPTED').length > 1;
-	return { gameid, result, isAcceptable };
+	const isFinished =
+		result.filter((finished) => finished.status === 'MATCHING_COMPLETED').length > 1;
+	return { gameid, result, isAcceptable, isFinished };
 };
